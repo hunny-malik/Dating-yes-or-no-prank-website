9f3039b5afccc988c3f5a97871df52e5ab916e9b
hunny-hacker <134684831+hunny-hacker@users.noreply.github.com>
2024-10-06 11:20:04 +0530

    Add files via upload

---
+++ website.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/') 
+def home():
+    return render_template('index.html')
+
+@app.route('/yes')
+def yes():
+    return render_template('yes.html')
+app.run(host='0.0.0.0', port=8080)
